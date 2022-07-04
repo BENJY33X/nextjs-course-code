a2e9f462120bbe2b456e2dafe486f9aee61bb6d6
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-04 16:00:16 +0100

    .

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -7,13 +7,13 @@ function Hero() {
     <section className={classes.hero}>
       <div className={classes.image}>
         <Image
-          src='/images/site/max.png'
-          alt='An image showing Max'
+          src='https://media-exp1.licdn.com/dms/image/D4D03AQEwfPycmPu2ng/profile-displayphoto-shrink_800_800/0/1649514634010?e=1662595200&v=beta&t=xw0LZAsx_uvE_XSzj75V6l384aOTzy6F-IEt_XRhH5Q'
+          alt='An image showing Abdessalam'
           width={300}
           height={300}
         />
       </div>
-      <h1>Hi, I'm Max</h1>
+      <h1>Hi, I'm Abdessalam</h1>
       <p>
         I blog about web development - especially frontend frameworks like
         Angular or React.
